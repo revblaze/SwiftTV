1c946523349f51f19ac1f5526da45e568713e18b
Justin Bush <revblaze@users.noreply.github.com>
2021-01-19 12:58:09 -0500

    Import assets

---
+++ iOS/SwiftTV/SwiftTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTVApp.swift
+//  SwiftTV
+//
+//  Created by Justin Bush on 2021-01-19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
